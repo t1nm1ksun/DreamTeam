b9acc92ac8f9ce81a5939e9db4ba33434ee56597
minsuk07672 <minsuk07672@konkuk.ac.kr>
2023-09-25 14:01:32 +0900

    9/24(2)

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
                 System.out.print("메뉴를 입력하세요 : ");
                 int classMenu = scanner.nextInt();
 
-                if (classMenu == 1 || classMenu == 3 || classMenu == 4) {
+                if (classMenu == 1) {
                     System.out.println("[수업 정보 리스트]");
                     // 수업 정보 리스트 출력 로직을 추가하세요.
                 } else if (classMenu == 2) {
@@ -49,8 +49,52 @@ public class Main {
                     }
                     System.out.println("수업 등록이 완료되었습니다.");
 
-                }
-                else {
+                } else if (classMenu==3) {
+                    System.out.println("[3.수업 편집을 선택하셨습니다.]");
+                    System.out.println("[수업 리스트]");
+                    System.out.print("편집할 수업 코드를 입력해주세요 (*공백없이 숫자 4자리를 입력하세요*): ");
+                    int classcode_Edit = scanner.nextInt();
+
+                    if (classcode_Edit<9999 && classcode_Edit>0) {
+                        System.out.println("[1.수업 시간 변경]"); //아직은 하나만
+                        System.out.print("변경할 항목을 선택하세요: ");
+                        int classdata_Edit = scanner.nextInt();
+                        if(classdata_Edit==1){
+                            System.out.println("[1.수업 시간 변경을 선택하셨습니다.]");
+                            System.out.println("[1.월 수 금 2.화 목 토]");
+                            System.out.print("수업요일을 선택해주세요 : ");
+                            int day = scanner.nextInt();
+                            if(day!=1&&day!=2){
+                                System.out.println("오류 : 잘못된 입력입니다. >> 1,2중 하나의 숫자를 선택해 주세요.");
+                            }
+                            System.out.println("[1.14:00-16:00 2.16:00-18:00 3.18:00-20:00 4.20:00-22:00]");
+                            System.out.print("수업시간을 선택해주세요 : ");
+                            int class_Time= scanner.nextInt();
+                            if (class_Time <= 1 && class_Time >= 4){
+                                System.out.println("오류 : 잘못된 입력입니다. >> 1,2,3,4중 하나의 숫자를 선택해 주세요.");
+                            }
+                            System.out.println("수업 시간 변경이 완료되었습니다.");
+                        }
+                    } else {
+                        System.out.println("[오류 : 등록된 수업이 없거나 잘못된 입력입니다.]");
+                    }
+                    System.out.println("[1.월 수 금 2.화 목 토]");
+                    System.out.print("수업요일을 선택해주세요 : ");
+
+                } else if (classMenu==4) {
+                    System.out.println("[4.수업 삭제를 선택하셨습니다.]");
+                    System.out.println("[수업리스트]");
+                    System.out.print("삭제할 수업 코드를 입력하세요 (*공백없이 숫자 4자리를 입력하세요*): ");
+                    int classcode_Delete= scanner.nextInt();
+                    if (classcode_Delete<9999 && classcode_Delete>0){
+                        System.out.println("수업 삭제가 완료되었습니다.");
+                    }
+                    else {
+                        System.out.println("[오류 : 등록된 수업이 없거나 잘못된 입력입니다.]");
+                        System.out.println("삭제할 수업 코드를 입력하세요 (*공백없이 숫자 4자리를 입력하세요*): ");
+                    }
+
+                } else {
                     System.out.println("[오류 : 잘못된 입력입니다. >> 1,2,3,4중 하나의 숫자를 선택해 주세요.]");
                 }
             } else if (choice == 2) {
@@ -88,12 +132,12 @@ public class Main {
                     System.out.println("[1.이름 2.전화번호 3.듣는수업]");
                     System.out.print("변경하고 싶은 학생 정보를 선택하세요 : ");
 //                    scanner.nextLine();
-                    int data_Edit = scanner.nextInt();
+                    int studentdata_Edit = scanner.nextInt();
                     System.out.println("[오류 : 잘못된 입력입니다. >> 1,2,3중 하나의 숫자를 선택해 주세요.]");
                     System.out.println("[1.이름 2.전화번호 3.듣는수업]");
                     System.out.print("변경하고 싶은 학생 정보를 선택하세요 : ");
-                    data_Edit = scanner.nextInt();
-                    if (data_Edit==1){
+                    studentdata_Edit = scanner.nextInt();
+                    if (studentdata_Edit==1){
                         System.out.println("[1.이름변경을 선택하셨습니다.]");
                         System.out.print("변경하려는 이름을 입력해 주세요 : ");
                         scanner.nextLine();
