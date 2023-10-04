ef9a83f12b6ec4939a549c966b18a8b4bad4bd8e
minsuk07672 <minsuk07672@konkuk.ac.kr>
2023-10-04 17:32:15 +0900

    CommonPatternError

--- src/CommonPatternError.java
+++ src/CommonPatternError.java
@@ -1,9 +1,16 @@
 public class CommonPatternError {
-    public static final String PHONE = "[오류: 잘못된 입력입니다. (*공백이나 '-' 없이 11개의 숫자를 한번에 입력하세요*)]"; //전화번호 에러 메세지
+    /** 전화번호 에러 메세지*/
+    public static final String PHONE = "[오류: 잘못된 입력입니다. (*공백이나 '-' 없이 11개의 숫자를 한번에 입력하세요*)]";
+    /** 수업 요일 에러 메세지*/
     public static final String LECTURE_DATE = "[오류: 잘못된 입력입니다. (*1,2중 하나의 숫자를 입력하세요.*)]"; //수업 요일 에러 메세지
+    /** 수업 시간 에러 메세지*/
     public static final String LECTURE_TIME = "[오류: 잘못된 입력입니다. (*1,2,3,4중 하나의 숫자를 입력하세요.*)]"; //수업 시간 에러 메세지
-    public static final String LECTURE_ID = "[오류: 잘못된 입력입니다. (*올바른 형식으로 입력하세요.*)]"; //수업 아이디 에러 메세지
+    /** 수업 아이디 에러 메세지 */
+    public static final String LECTURE_ID = "[오류: 잘못된 입력입니다. (*올바른 형식으로 입력하세요.*)]";
+    /**선생님 아이디 에러 메세지*/
     public static final String TEACHER_ID = "[오류: 잘못된 입력입니다. (*올바른 형식으로 입력하세요.*)]"; //선생님 아이디 에러 메세지
+    /** 학생 아이디 에러 메세지*/
     public static final String STUDENT_ID = "[오류: 잘못된 입력입니다. (*올바른 형식으로 입력하세요.*)]"; //학생 아이디 에러 메세지
+    /** 학생 이름 에러 메세지*/
     public static final String STUDENT_NAME = "[오류: 잘못된 입력입니다. (*2~10글자의 한글을 입력하세요.*)]"; //학생 이름 에러 메세지
 }
