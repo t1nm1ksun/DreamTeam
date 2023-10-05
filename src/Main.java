5186eeee9fef07fe5011c0992e512738358496e3
minsuk07672 <minsuk07672@konkuk.ac.kr>
2023-10-05 11:23:45 +0900

    no message

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     //전역변수 선언 =================================================================
-<<<<<<< HEAD
+
     public static String choice; //메인 메뉴
     public static String classMenu; //수업 관리 메뉴
     public static String studentMenu; //학생 관리 메뉴
@@ -13,16 +13,7 @@ public class Main {
         LectureManager Lm = new LectureManager();
         StudentManager Sm = new StudentManager();
 //        Lm.deleteLecture("1000");
-=======
-    public static int choice; //메인 메뉴
-    public static int classMenu; //수업 관리 메뉴
-    public static int studentMenu; //학생 관리 메뉴
 
-
-    //=============================================================================
-    public static void main(String[] args) {
-        LectureManager Lm = new LectureManager();
->>>>>>> 90b175db37dad50a2cbdaec0a06c85a3ab7fcff7
         Lm.addLecture();
         Lm.displayLectures();
         ScannerInstance instance = ScannerInstance.getInstance();
