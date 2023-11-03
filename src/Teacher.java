a800c0211bd00f470582cfc3b3cfb5328a8b0746
whddltjdwhd <castle_bell@Castle-Book.local>
2023-11-03 16:17:16 +0900

    기획서에 맞게 csv변경 작업 시작 선생님 반영

--- src/Teacher.java
+++ src/Teacher.java
@@ -8,7 +8,7 @@ public class Teacher {
     private final List<TimeTable> timeTables;
 
     //과목코드 추가!
-    public Teacher(String name, String code, String subjectCode, List<TimeTable> timeTables) {
+    public Teacher(String code, String name, String subjectCode, List<TimeTable> timeTables) {
         this.code = code;
         this.name = name;
         this.subjectCode = subjectCode;
