ddb9c159631c8ffe5f196adc763f39d91aa4f8a0
ckkim817 <ckkim817@konkuk.ac.kr>
2023-11-06 00:27:32 +0900

    보기 좋게 수정

--- src/LectureManager.java
+++ src/LectureManager.java
@@ -148,10 +148,10 @@ public class LectureManager implements BaseManager {
             }
 
             ScannerUtils.print(
-                    "[" + t.getCode() + "번 타임테이블: " + t.getRoomId() + "강의실 " + lectureDay + " " + t.showLectureTime()
-                            + "] ", false);
+                    "[" + t.getCode() + "번 타임테이블: " + t.getRoomId() + "번 강의실 / " + lectureDay + " / "
+                            + t.showLectureTime()
+                            + "] ", true);
         }
-        ScannerUtils.print("", true);
     }
 
     public boolean deleteLecture() {
@@ -319,8 +319,7 @@ public class LectureManager implements BaseManager {
                         timetable.add(newTimetable); // "timetable" 객체에 "newTimetable" 객체를 전달
                         teacherNow.addTimetable(newTimetable); // "teacherNow" 객체에 "newTimetable" 객체를 전달
                         ScannerUtils.print("해당 시간에 성공적으로 추가되었습니다.", true);
-                    }
-                    else {
+                    } else {
                         ScannerUtils.print("해당 타임에는 이미 수업이 존재합니다.", true);
                     }
                 } else {
@@ -366,7 +365,7 @@ public class LectureManager implements BaseManager {
         if (Main.timetableManager.getTimetable().isEmpty()) {
             ScannerUtils.print("등록되어 있는 수업이 없습니다.", true);
         } else {
-            ScannerUtils.print("변경할 수업 코드를 선택하시오", true);
+            ScannerUtils.print("변경할 수업 코드를 입력하세요: ", false);
             LectureEditMenuHandler.input = ScannerUtils.scanWithPattern(CommonPattern.LECTURE_CODE,
                     CommonPatternError.LECTURE_CODE);
             while (Integer.parseInt(LectureEditMenuHandler.input) > LectureManager.maxCode) {
@@ -376,12 +375,13 @@ public class LectureManager implements BaseManager {
             }
 
             //타임 테이블 출력
-            ScannerUtils.print("변경할 요일의 타임테이블 코드를 입력하세요 (예시: 6000): ", true);
-
             displayTimetable(hasLecture(LectureEditMenuHandler.input));
+
+            ScannerUtils.print("변경할 타임의 타임테이블 코드를 입력하세요 (ex: 6000): ", false);
+
             String code_toEdit = ScannerUtils.scanWithPattern(CommonPattern.TIMETABLE_CODE,
                     CommonPatternError.TIMETABLE_CODE); // TODO : 바꿀 타임테이블 입력받기 (민석)
-            //받아'
+            //받아
             TimeTable tb_toEdit = null;
 
             for (TimeTable tab : hasLecture(LectureEditMenuHandler.input).getTimetable()) {
@@ -395,10 +395,10 @@ public class LectureManager implements BaseManager {
             String day = tb_toEdit.getLectureDays();
             String time = tb_toEdit.getLectureTime();
 
-            ScannerUtils.print("1) 수업 요일 변경   2) 수업 시간 변경", true);
-            ScannerUtils.print("변경할 정보를 선택하세요 : ", true);
-            int data_toEdit = ScannerUtils.scanWithPatternIntegerForMenu(CommonPattern.TWO_CHOICE,
-                    CommonPatternError.TWO_CHOICE);
+            ScannerUtils.print("1) 수업 요일 변경    2) 수업 시간 변경", true);
+            ScannerUtils.print("변경할 정보를 선택하세요: ", false);
+            int data_toEdit = Integer.parseInt(
+                    ScannerUtils.scanWithPattern(CommonPattern.TWO_CHOICE, CommonPatternError.TWO_CHOICE));
             switch (data_toEdit) {
                 case 1: {
                     ScannerUtils.print("수업 요일 변경을 선택하셨습니다.", true);
@@ -412,7 +412,7 @@ public class LectureManager implements BaseManager {
                     }
                     ScannerUtils.print("", true);
 
-                    ScannerUtils.print("수업 요일을 선택해 주세요", true);
+                    ScannerUtils.print("수업 요일을 선택해 주세요: ", false);
                     day = ScannerUtils.scanWithPattern(CommonPattern.LECTURE_DATE, CommonPatternError.LECTURE_DATE);
                     break;
                 }
@@ -428,7 +428,7 @@ public class LectureManager implements BaseManager {
                         ScannerUtils.print(timeOption + " ", false);
                     }
                     ScannerUtils.print("", true);
-                    ScannerUtils.print("수업 시간을 입력해 주세요", true);
+                    ScannerUtils.print("수업 시간을 입력해 주세요: ", false);
                     time = ScannerUtils.scanWithPattern(CommonPattern.LECTURE_TIME, CommonPatternError.LECTURE_TIME);
 
                     break;
