de5c33f684e6d8b5a4976b8d538751711ca14885
rldnd <gi981226@gmail.com>
2023-11-24 15:34:11 +0900

    feat(csv-validator): [WIP] validateCsvRef

--- src/CsvValidator.java
+++ src/CsvValidator.java
@@ -12,25 +12,48 @@ public class CsvValidator {
         return Read.validateCSVListFormat(Arrays.asList(Main.subjectManager, Main.timetableManager, Main.lectureManager, Main.lectureroomManager, Main.studentManager, Main.teacherManager));
     }
 
-    static boolean validateRef(){
-        if (!Read.validateCSVRef(Main.timetableManager, Main.lectureroomManager, "1", "0")) return false;
-        if (!Read.validateCSVRef(Main.lectureManager, Main.timetableManager, "+5", "0")) return false;
-        if (!Read.validateCSVRef(Main.timetableManager, Main.lectureroomManager, "1", "0")) return false;
-        if (!Read.validateCSVRef(Main.lectureManager, Main.subjectManager, "1", "0")) return false;
-        if (!Read.validateCSVRef(Main.lectureManager, Main.teacherManager, "2", "0")) return false;
-        if (!Read.validateCSVRef(Main.lectureManager, Main.timetableManager, "+5", "0")) return false;
-        if (!Read.validateCSVRef(Main.teacherManager, Main.subjectManager, "+2", "0")) return false;
-        if (!Read.validateCSVRef(Main.studentManager, Main.lectureManager, "+3", "0")) return false;
-        //Todo 기웅햄 부탁드립니다!
-        if (!Read.validateCSVRef(Main.divisionManager, Main.divisionManager, " ", " ")) return false;
-        return true;
-    }
-
     static boolean validateInformations(){
         if(!Read.validatePhoneNumberDupliacated(Arrays.asList(Main.studentManager))) return false;
         if(!Read.validateTimetableIdDupliacated(Arrays.asList(Main.lectureManager, Main.teacherManager))) return false;
         if(!Read.validateTimetableInfoDuplicated(Main.timetableManager)) return false;
         if(!Read.validateLectureHasOverStudents(Main.lectureManager)) return false;
         return true;
+    }
+
+    static boolean validateRef(){
+        boolean isValidateRefFromTeacher = validateRefFromTeacher();
+        boolean isValidateRefFromStudent = validateRefFromStudent();
+        boolean isValidateRefFromTimetable = validateRefFromTimetable();
+        boolean isValidateRefFromDivision = validateRefFromDivision();
+        boolean isValidateRefFromLecture = validateRefFromLecture();
+
+        return (isValidateRefFromTeacher && isValidateRefFromStudent && isValidateRefFromTimetable && isValidateRefFromDivision && isValidateRefFromLecture);
+    }
+
+    static boolean validateRefFromTeacher(){
+        boolean teacherToSubjects = Read.validateCSVRef(Main.teacherManager, Main.subjectManager, "+2", "0");
+        return teacherToSubjects;
+    }
+
+    static boolean validateRefFromStudent(){
+        boolean studentToDivisions = Read.validateCSVRef(Main.studentManager, Main.divisionManager, "+3", "0");
+        return studentToDivisions;
+    }
+
+    static boolean validateRefFromTimetable(){
+        boolean timetableToLectureroom = Read.validateCSVRef(Main.timetableManager, Main.lectureroomManager, "1", "0");
+        return timetableToLectureroom;
+    }
+
+    static boolean validateRefFromDivision(){
+        boolean divisionToLecture = Read.validateCSVRef(Main.divisionManager, Main.lectureManager, "1","0");
+        boolean divisionToTeacher = Read.validateCSVRef(Main.divisionManager, Main.teacherManager, "2","0");
+        boolean divisionToTimetables = Read.validateCSVRef(Main.divisionManager, Main.timetableManager, "+4", "0");
+        return (divisionToLecture && divisionToTeacher && divisionToTimetables);
+    }
+
+    static boolean validateRefFromLecture(){
+        boolean lectureToSubject = Read.validateCSVRef(Main.lectureManager, Main.subjectManager, "2", "0");
+        return lectureToSubject;
     }
 }
